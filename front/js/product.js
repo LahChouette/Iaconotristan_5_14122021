1be184058ef5308bc6684456638579c79c4f1f2d
Iacono Tristan <boulbi38220@gmail.com>
2022-02-06 14:31:24 +0100

    soutenance mise en forme

--- front/js/product.js
+++ front/js/product.js
@@ -11,91 +11,91 @@ function getArticleId() {
 // ajout des élément de l'API demandé + ajout événement bouton panier + locastorage //
 function getArticle(articleId) {
     fetch(`http://localhost:3000/api/products/${articleId}`)
-    .then(reponse => reponse.json())
-    .then(article => {
-        
-        let element = document.querySelector(".item");
-        
-        // element image, titre, prix, description, couleurs //
-       
-        element.querySelector(".item__img").insertAdjacentHTML("afterbegin", `<img src="${article.imageUrl}" alt="Photo d'un canapé ${article.name}">`);
-        /* let image = document.createElement('img')
-         image.src = article.imageUrl
-         image.alt = article.name
-         element.querySelector(".item__img").appendChild(image)*/
-        
-        
-        element.querySelector('#title').textContent = article.name
-      
-        element.querySelector('#price').textContent = article.price
-        
-        element.querySelector('#description').textContent = article.description
-        
-        element.querySelector("#colors").insertAdjacentHTML("beforeend", article.colors.map(color => `<option value="${color}">${color}</option>`));
+        .then(reponse => reponse.json())
+        .then(article => {
+
+            let element = document.querySelector(".item");
+
+            // element image, titre, prix, description, couleurs //
+
+            element.querySelector(".item__img").insertAdjacentHTML("afterbegin", `<img src="${article.imageUrl}" alt="Photo d'un canapé ${article.name}">`);
+            /* let image = document.createElement('img')
+             image.src = article.imageUrl
+             image.alt = article.name
+             element.querySelector(".item__img").appendChild(image)*/
 
 
-        //event bouton panier//
-        document.querySelector("#addToCart").addEventListener("click", (event)=> {
-            
-            // choix de l'utilisateur couleur et quantiter //
-            const couleurSelect = document.querySelector("#colors").value;
-            const quantiteSelect = document.querySelector("#quantity").value;
+            element.querySelector('#title').textContent = article.name
 
-            // impossible de choisir une quantité a 0 ou plus de 100 //
-            if (quantiteSelect > 0 && quantiteSelect <=100 && quantiteSelect != 0){
-                
-                // recuperation du formulaire //
-                let optionsPanier = {
-                    id: article._id,
-                    img: article.imageUrl,
-                    nom: article.name,
-                    description: article.description,
-                    prix: article.price,
-                    couleur: couleurSelect,
-                    quantite: quantiteSelect,
-                }
-            
-                // popup de confimation //
-                const popupConfirmation =() =>{
-                if(window.confirm(`Votre commande de ${quantiteSelect} ${article.name} ${couleurSelect} est ajoutée au panier Pour consulter votre panier, cliquez sur OK`)){
-                    window.location.href ="cart.html";
-                }
-                }
-            
-                // LocalStorage //
-                let produitLocalStorage = JSON.parse(localStorage.getItem("panier"));
-            
-                if (produitLocalStorage) {
-                
-                    const resultatFind = produitLocalStorage.find(
-                        (i) => i.id === article._id && i.couleur === couleurSelect);
-                    
+            element.querySelector('#price').textContent = article.price
+
+            element.querySelector('#description').textContent = article.description
+
+            element.querySelector("#colors").insertAdjacentHTML("beforeend", article.colors.map(color => `<option value="${color}">${color}</option>`));
+
+
+            //event bouton panier//
+            document.querySelector("#addToCart").addEventListener("click", (event) => {
+
+                // choix de l'utilisateur couleur et quantiter //
+                const couleurSelect = document.querySelector("#colors").value;
+                const quantiteSelect = document.querySelector("#quantity").value;
+
+                // impossible de choisir une quantité a 0 ou plus de 100 //
+                if (quantiteSelect > 0 && quantiteSelect <= 100 && quantiteSelect != 0) {
+
+                    // recuperation du formulaire //
+                    let optionsPanier = {
+                        id: article._id,
+                        img: article.imageUrl,
+                        nom: article.name,
+                        description: article.description,
+                        prix: article.price,
+                        couleur: couleurSelect,
+                        quantite: quantiteSelect,
+                    }
+
+                    // popup de confimation //
+                    const popupConfirmation = () => {
+                        if (window.confirm(`Votre commande de ${quantiteSelect} ${article.name} ${couleurSelect} est ajoutée au panier Pour consulter votre panier, cliquez sur OK`)) {
+                            window.location.href = "cart.html";
+                        }
+                    }
+
+                    // LocalStorage //
+                    let produitLocalStorage = JSON.parse(localStorage.getItem("panier"));
+
+                    if (produitLocalStorage) {
+
+                        const resultatFind = produitLocalStorage.find(
+                            (i) => i.id === article._id && i.couleur === couleurSelect);
+
                         // Si le produit commandé est déjà dans le panier //
                         if (resultatFind) {
-                        let newQuantite =
-                        parseInt(optionsPanier.quantite) + parseInt(resultatFind.quantite);
-                        resultatFind.quantite = newQuantite;
-                        localStorage.setItem("panier", JSON.stringify(produitLocalStorage));
-                        popupConfirmation();
-                        
-                        // Si le produit commandé n'est pas dans le panier //
+                            let newQuantite =
+                                parseInt(optionsPanier.quantite) + parseInt(resultatFind.quantite);
+                            resultatFind.quantite = newQuantite;
+                            localStorage.setItem("panier", JSON.stringify(produitLocalStorage));
+                            popupConfirmation();
+
+                            // Si le produit commandé n'est pas dans le panier //
                         } else {
+                            produitLocalStorage.push(optionsPanier);
+                            localStorage.setItem("panier", JSON.stringify(produitLocalStorage));
+                            popupConfirmation();
+
+                        }
+                        // Si le panier est vide //
+                    } else {
+                        produitLocalStorage = [];
                         produitLocalStorage.push(optionsPanier);
                         localStorage.setItem("panier", JSON.stringify(produitLocalStorage));
                         popupConfirmation();
-                        
-                        }
-                    // Si le panier est vide //
+                    }
                 } else {
-                    produitLocalStorage =[];
-                    produitLocalStorage.push(optionsPanier);
-                    localStorage.setItem("panier", JSON.stringify(produitLocalStorage));
-                    popupConfirmation();
+                    alert('Veuillez choisir la quantité du produit.');
                 }
-            } else {
-                alert('Veuillez choisir la quantité du produit.');
-            }   
+            })
         })
-    })
-    .catch(err => console.log(err))
+        .catch(err => console.log(err))
 }
